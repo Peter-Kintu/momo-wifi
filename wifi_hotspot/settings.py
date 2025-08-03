dce1846f779011afac036dc1688cf568686343f4
Kintu Peter <kintupeter721@gmail.com>
2025-08-03 12:56:42 +0300

    Fix: settings

--- wifi_hotspot/settings.py
+++ wifi_hotspot/settings.py
@@ -44,8 +44,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rest_framework',
-    'core', # Assuming your Django application is named 'core'
+    'core',
 ]
 
 MIDDLEWARE = [
@@ -63,7 +62,7 @@ ROOT_URLCONF = 'wifi_hotspot.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -125,6 +124,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
@@ -133,14 +133,10 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # MTN MoMo API settings
 # These variables are loaded from the .env file
-MOMO_CLIENT_ID = os.getenv('MOMO_CLIENT_ID')
 MOMO_COLLECTIONS_API_KEY = os.getenv('MOMO_COLLECTIONS_API_KEY')
+COLLECTION_PRIMARY_KEY = os.getenv('COLLECTION_PRIMARY_KEY')
 MOMO_API_USER_ID = os.getenv('MOMO_API_USER_ID')
 MOMO_API_KEY = os.getenv('MOMO_API_KEY')
 MOMO_TARGET_ENVIRONMENT = os.getenv('MOMO_TARGET_ENVIRONMENT')
 MOMO_CALLBACK_URL = os.getenv('MOMO_CALLBACK_URL')
-
-# MikroTik API settings
-MIKROTIK_HOST = os.getenv('MIKROTIK_HOST')
-MIKROTIK_USER = os.getenv('MIKROTIK_USER')
-MIKROTIK_PASSWORD = os.getenv('MIKROTIK_PASSWORD')
+MOMO_CURRENCY = os.getenv('MOMO_CURRENCY', 'UGX')
